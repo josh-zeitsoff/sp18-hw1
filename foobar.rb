5191314545e49c1cd7825f26f160d3df26773da5
Joshua Zeitsoff <jzeitsoff@berkeley.edu>
2018-02-10 00:47:24 -0800

    Submit hw1

--- foobar.rb
+++ foobar.rb
@@ -1,6 +1,12 @@
 class Foobar
 
   def self.baz(a)
+    a.map! {|e| e.to_i}
+    a.map! {|e| e += 2}
+    a.select! {|e| e.even?}
+    a = a.uniq
+    a.select! {|e| e < 10}
+    a.sum
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,14 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  (a + b)**2
 
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
+  a.map! {|e| e += 1 }.sort
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
-
+  first_name + " " + last_name
 end
 
 def blockin_time(a)
@@ -48,5 +46,5 @@ def scrabble(word)
     y: 4,
     z: 10,
   }
-  # Q5 CODE HERE
+  word.split("").map!{|e| values[e.to_sym]}.sum
 end
